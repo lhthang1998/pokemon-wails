62394a5db5d5ecc6f714d237c86cf4a435fd30e3
lhthang1998 <lhthang.1998@gmail.com>
2020-09-16 21:51:31 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <Header></Header>
+  );
+}
+
+export default App;
