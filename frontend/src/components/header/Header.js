0ddb11d38dced6c487a8cc5e2f9020e012998017
lhthang1998 <lhthang.1998@gmail.com>
2020-09-16 22:01:58 +0700

    add react-bootstrap

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,11 +1,12 @@
 
-import React , { FunctionComponent } from 'react'
+import React, { FunctionComponent } from 'react'
 import './Header.css'
+import {Navbar} from 'react-bootstrap'
 
-const Header:FunctionComponent=()=>{
-    return(
-        <Navbar>
-            <Navbar.Brand>POKEMON</Navbar.Brand>
+const Header: FunctionComponent = () => {
+    return (
+        <Navbar bg="light" expand="lg" >
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         </Navbar>
     );
 }
